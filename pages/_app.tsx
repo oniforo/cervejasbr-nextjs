15f58e9a92f8b9645eb90314d16aa4bcc8fd971d
Gabriel Orofino <g.orofino@protonmail.com>
2026-08-20 22:02:09 -0300

    feat: add Commerce.js client and cart state
    Adds a shared Commerce.js SDK client and a React context/reducer for
    cart state, retrieved on mount and wired into the app via CartProvider.
    Also ignores the local Chec env file.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { CartProvider } from '../context/cart'
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <CartProvider>
+      <Component {...pageProps} />
+    </CartProvider>    
+  )
 }
 
 export default MyApp
